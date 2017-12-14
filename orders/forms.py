1ad5aed53cb6ef9653d55e8aea14892d7d58c678
LudwigOtto <ykao@cs.stonybrook.edu>
2017-12-14 14:22:29 -0500

    Update 004

--- orders/forms.py
+++ orders/forms.py
@@ -1,21 +1,17 @@
 from django import forms
 
 class ShipForm(forms.Form):
-    c_id = forms.IntegerField(label='User ID', widget=forms.NumberInput)
-    email = forms.EmailField(label='E-Mail', widget=forms.EmailInput)
-    phone = forms.CharField(label='Phone #', widget=forms.TextInput)
+    ship_type = forms.CharField(label='Shipment Type', widget=forms.TextInput)
+    ship_detail = forms.CharField(label='Shipment Detail', widget=forms.TextInput)
     address = forms.CharField(label='Address', widget=forms.TextInput)
 
     update = forms.BooleanField(required=False,
                                 initial=False,
                                 widget=forms.HiddenInput)
 class PayForm(forms.Form):
-    c_id = forms.IntegerField(label='User ID', widget=forms.NumberInput)
-    firstN = forms.CharField(label='First Name', widget=forms.TextInput)
-    lastN = forms.CharField(label='Last Name', widget=forms.TextInput)
-    email = forms.EmailField(label='E-Mail', widget=forms.EmailInput)
-    phone = forms.CharField(label='Phone #', widget=forms.TextInput)
-    address = forms.CharField(label='Address', widget=forms.TextInput)
+    pay_Type = forms.CharField(label='Payment Type', widget=forms.TextInput)
+    card_num = forms.CharField(label='Card Number', widget=forms.TextInput)
+    card_date = forms.DateField(label='Expire Date', widget=forms.DateInput)
 
     update = forms.BooleanField(required=False,
                                 initial=False,
