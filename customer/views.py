1a20328625c1245f87204db65f3c0ad83b7c527c
LudwigOtto <ykao@cs.stonybrook.edu>
2017-12-13 22:46:10 -0500

    Update 002 171213

--- customer/views.py
+++ customer/views.py
@@ -1,9 +1,11 @@
+from django.shortcuts import render, get_object_or_404
 from django.shortcuts import render
 from .models import Customer
 
 # Create your views here.
-def customer_list(request):
-    customers = Customer.objects.all()
+def customer_list(request, c_id = 1):
+    #customers = Customer.objects.all()
+    customer = get_object_or_404(Customer, customer_id = c_id)
     return render(request,
                   'customer/list.html',
-                  {'customers': customers})
+                  {'customer': customer})
